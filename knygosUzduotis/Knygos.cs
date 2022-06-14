1c38dbebeac0d4aebb92d51ac5bfc1d442c4a862
Znamenskas <zvilandas@gmail.com>
2022-06-14 10:16:24 +0300

    Konvertuoja string eilute i knygos objekta

---
+++ knygosUzduotis/Knygos.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace knygosUzduotis
+{
+    public class Knygos
+    {
+        private string isbn;
+        private string pavdinimas;
+        private string autorius;
+        private int leidimoMetai;
+        private string leidejas;
+        private string salis;
+        private decimal kaina;
+
+        
+        // KONSTRUKTORIAI.
+
+        public Knygos(string isbn, string pavdinimas, string autorius, int leidimoMetai, string leidejas, string salis, decimal kaina)
+        {
+            this.Isbn = isbn;
+            this.Pavdinimas = pavdinimas;
+            this.Autorius = autorius;
+            this.LeidimoMetai = leidimoMetai;
+            this.Leidejas = leidejas;
+            this.Salis = salis;
+            this.Kaina = kaina;
+        }
+
+        public Knygos()
+        {
+        }
+        // GETERIAI SETERIAI.
+
+        public string Isbn { get => isbn; set => isbn = value; }
+        public string Pavdinimas { get => pavdinimas; set => pavdinimas = value; }
+        public string Autorius { get => autorius; set => autorius = value; }
+        public int LeidimoMetai { get => leidimoMetai; set => leidimoMetai = value; }
+        public string Leidejas { get => leidejas; set => leidejas = value; }
+        public string Salis { get => salis; set => salis = value; }
+        public decimal Kaina { get => kaina; set => kaina = value; }
+
+        public override string ToString()
+        {
+            // Overridintas ToString() metodas.
+
+            string tekstas = " isbn: " + isbn + "; Pavadinimas: " + pavdinimas + "; Autorius: " + autorius + "; Leidimo metai: " + leidimoMetai + "; Leidejas: " + leidejas + "; Salis: " + salis + "; Kaina: " + kaina;
+
+            return tekstas;
+        }
+    }
+}
--- knygosUzduotis/Program.cs
+++ knygosUzduotis/Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace knygosUzduotis
 {
     class Program
     {
+        // CIA YRA PASTOVUS KINTAMIEJI / REIKSMES (konstantos) , pvz.: failo pavadinimas.
+
+        const string FailoPavadinimas = "knygos.csv";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Knygos knygos = new Knygos("95 - 6351 - 575 - 7", "El Sanción Creyente", "Turner Sandoval Loya", 1944," Línea Editorial","Chile", 56.8m);
+
+            //Console.WriteLine(knygos.ToString());
+               NuskaitytiKnygosIsFailo();
+        }
+
+        static List<Knygos> NuskaitytiKnygosIsFailo()
+        {
+            List<Knygos> knygos = new List<Knygos>();
+            string[] eilutes = System.IO.File.ReadAllLines(FailoPavadinimas);
+
+
+
+            foreach (string eilute in eilutes.Skip(1).ToArray())
+            {
+                //Console.WriteLine(eilute);
+
+                knygos.Add(KonvertuojaEiluteIknygos(eilute));
+                
+            }
+            return knygos;
+
+
+        }
+
+        static Knygos KonvertuojaEiluteIknygos(string eilute) 
+        {
+            string[] stulpeliai = eilute.Split(",");
+
+            Knygos knygos = new Knygos();
+
+            Console.WriteLine(stulpeliai[0]);
+            Console.WriteLine(stulpeliai[1]);
+            Console.WriteLine(stulpeliai[2]);
+            Console.WriteLine(stulpeliai[3]);
+            Console.WriteLine(stulpeliai[4]);
+            Console.WriteLine(stulpeliai[5]);
+            Console.WriteLine(stulpeliai[6]);
+
+            knygos.Isbn =  stulpeliai[0];
+            knygos.Pavdinimas = stulpeliai[1];
+            knygos.Autorius = stulpeliai[2];
+            knygos.LeidimoMetai = Convert.ToInt32(stulpeliai[3]);
+            knygos.Leidejas = stulpeliai[4];
+            knygos.Salis = stulpeliai[5];
+            knygos.Kaina = Convert.ToDecimal(stulpeliai[6]);
+
+            return knygos;
         }
     }
 }
